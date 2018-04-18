99b7998160c84009fd80595743457d638fd18a4b
Nik <nerdvestor@gmail.com>
2018-04-17 17:20:15 -0700

    finish

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyBynEQyeAG-6RpDDdcMreixSLiqPaDtP9Q",
+    authDomain: "notes-app-4a59c.firebaseapp.com",
+    databaseURL: "https://notes-app-4a59c.firebaseio.com",
+    projectId: "notes-app-4a59c",
+    storageBucket: "notes-app-4a59c.appspot.com",
+    messagingSenderId: "293074727976"
+  };
